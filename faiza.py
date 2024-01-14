c592423af3e0aba8a12ac8774d5c98643ab91bd3
faiza101111 <156578801+faiza101111@users.noreply.github.com>
2024-01-14 15:40:42 +0500

    Add files via upload

---
+++ faiza.py
@@ -0,0 +1,23 @@
+print("""
+addition
+subtraction
+multiplication
+subtraction""")
+num1=int(input("enter the value num1"))
+num2=int(input("enter the value num2"))
+opr=input("enter the opr...")
+if opr == "+":
+    print(num1+num2)
+elif opr == "-":
+    print(num1-num2)
+elif opr == "*":
+    print(num1*num2)
+elif opr == "/":
+    print(num1/num2)
+else:
+    print("invalid opr....")
+
+    
+    
+     
+     
